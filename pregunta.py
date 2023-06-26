4fef8721132294b45b8cdb78d40ef70dc5eea4a8
Lema25 <56377966+Lema25@users.noreply.github.com>
2023-06-26 04:38:32 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,6 +10,8 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import numpy as np
+import re
 
 
 def ingest_data():
@@ -17,5 +19,32 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    
+    with open("clusters_report.txt",'r') as file:
+        Datos= file.readlines()
+    listado = list()
+    titulos = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+    Datos = Datos[4:]
+    
+    for line in Datos:
+        Palabras = line.split()
+        if len(Palabras) != 0:
+            if Palabras[0].isdigit():
+                listado.append(Palabras[0:3])
+                listado[-1].append(' '.join(Palabras[4:]))
+                listado[-1][-1] += ' '
+            elif Palabras[0].isdigit():
+                listado[-1][-1] += ' '.join(Palabras)
+                listado[-1][-1] += ' '
+        
+    for line in listado:
+        line[-1] = line[-1].rstrip('. ')
+        line[0] = int(line[0])
+        line[1] = int(line[1])
+        line[2] = line[2].replace(',','.')
+        line[2] = float(line[2])
+
+    
+    df = pd.DataFrame(listado,columns=titulos)
 
     return df
